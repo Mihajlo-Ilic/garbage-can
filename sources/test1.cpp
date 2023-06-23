181a144676dd45efc7afc6f4d28a5748f7a9e2b4
Mihajlo-Ilic <herb.m90@gmail.com>
2023-06-23 19:42:30 +0200

    Added examples. Multiple tests are possible in multiple directories

---
+++ sources/test1.cpp
@@ -0,0 +1,8 @@
+#include "../includes/TestSuiteMeta.hpp"
+
+#include <iostream>
+
+Test(First_test)
+{
+    std::cout<< "Hello from first test"<<std::endl;
+}
---
+++ sources/test2.cpp
@@ -0,0 +1,13 @@
+#include "../includes/TestSuiteMeta.hpp"
+
+#include <iostream>
+
+Test(second_test)
+{
+    std::cout<<"Hello from second test"<<std::endl;
+}
+
+Test(third_test)
+{
+    std::cout<<"Hello from third test"<<std::endl;
+}
